d49d88a5f19e17fb77810837ddc8e1496015ace2
Safayat Borhan <SAFBO@orsted.com>
2022-11-27 20:42:32 +0800

    Create service and factory classes.

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,94 @@
-﻿namespace FactoryMethod
+﻿/*
+ * Requirements:
+ * Imagine creation of a shopping cart system.
+ * At a certain moment, a discount can be applied.
+ * Discount can come up from two different crieteria. 
+ * One is discount code and another is country.
+ * Discount percentage is needed for this.
+ */
+
+namespace FactoryMethod;
+
+public abstract class DiscountService
 {
-   
+    public abstract int DiscountPercentage
+    {
+        get;
+    }
+
+    public override string ToString()
+    {
+        return GetType().Name;
+    }
+}
+
+public class CountryDiscountService : DiscountService
+{
+    private readonly string _countryIdentifier;
+
+    public CountryDiscountService(string countryIdentifier)
+    {
+        _countryIdentifier = countryIdentifier;
+    }
+
+    public override int DiscountPercentage
+    {
+        get
+        {
+            switch (_countryIdentifier)
+            {
+                case "BD":
+                    return 20;
+                default:
+                    return 10;
+            }
+        }
+    }
+}
+
+public class CodeDiscountService : DiscountService
+{
+    private readonly Guid _code;
+
+    public CodeDiscountService(Guid code)
+    {
+        _code = code;
+    }
+
+    public override int DiscountPercentage => 15;
+}
+
+public abstract class DiscountFactory
+{
+    public abstract DiscountService CreateDiscountService();
+}
+
+public class CountryDiscountFactory : DiscountFactory
+{
+    private readonly string _countryIdentifier;
+
+    public CountryDiscountFactory(string countryIdentifier)
+    {
+        _countryIdentifier = countryIdentifier;
+    }
+
+    public override DiscountService CreateDiscountService()
+    {
+        return new CountryDiscountService(_countryIdentifier);
+    }
+}
+
+public class CodeDiscountFactory : DiscountFactory
+{
+    private readonly Guid _code;
+
+    public CodeDiscountFactory(Guid code)
+    {
+        _code = code;
+    }
+
+    public override DiscountService CreateDiscountService()
+    {
+        return new CodeDiscountService(_code);
+    }
 }
